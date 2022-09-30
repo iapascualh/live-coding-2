e578450c2bee7bab1af0af0cd81ff71b56045e41
Irving Alexis Pascual Hernandez <hpascual117gmail.com>
2022-09-30 00:13:24 -0500

    chore | se concreto el ejercio correctamente

--- starter-files/script.js
+++ starter-files/script.js
@@ -1,6 +1,47 @@
 // API
 const API_ENDPOINT = 'https://yesno.wtf/api';
+var div = document.getElementById("answer");
+var btn = document.getElementById("button");
+btn.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (validation()) {
+        getData();
+    }//if
+});//event
+const getData = () => {
+    let promesa = fetch("https://yesno.wtf/api", {
+        method: "GET"
+    });
+    promesa.then((response) => {
+        response.json().then((data) => {
+            console.log(data.answer);
+            div.innerText = data.answer;
+            console.log(typeof (data));
+            const cronometro = setTimeout(clear, 5000);
+        }).catch((error) => {
+            console.error(error);
+        });
+    }).catch((error) => {
+        console.log("error " + error);
+    })
+}//getData
 
+
+
+function clear() {
+    answer.innerText = "";
+    document.getElementById("input").value = '';
+}
+
+function validation() {
+    if (document.getElementById("input").value) {
+        return true;
+    } else {
+        console.log("favor de ingresar los datos requeridos");
+        return false;
+    }
+
+}
 /**
  * STEPS:
  *
